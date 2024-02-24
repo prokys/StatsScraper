59a576b0c6bd012f694e5e053c7c59cec9e7e2a2
Prokop Ferdan <prokes1@gmail.com>
2024-02-24 10:47:15 +0100

    added new dependecies and made app export scraped data to .xlsx

--- src/main/java/statsScraper/App.java
+++ src/main/java/statsScraper/App.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.FileOutputStream;
 import java.time.Duration;
 import java.util.List;
 
@@ -16,7 +18,8 @@ import java.util.List;
 public class App
 {
     public static void main( String[] args ){
-        try {
+        try (Workbook workbook = new XSSFWorkbook()){
+            Sheet sheet = workbook.createSheet("Proky");
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless");
             WebDriver driver = new ChromeDriver(options);
@@ -24,17 +27,34 @@ public class App
             Actions actions = new Actions(driver);
             actions.scrollToElement(driver.findElement(By.xpath("//img[@class='ProfilePerson--Table-icon plus']"))).perform();
             actions.moveToElement(driver.findElement(By.xpath("//img[@class='ProfilePerson--Table-icon plus']"))).perform();
+
             new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@class='ProfilePerson--Table-icon plus']"))).click();
             List<WebElement> tbodyElements = driver.findElements(By.cssSelector("tbody.js-collButton-target"));
 
+
+            int rowNum = 0;
             for (WebElement tbodyElement : tbodyElements) {
                 List<WebElement> trElements = tbodyElement.findElements(By.tagName("tr"));
                 for (WebElement trElement : trElements) {
                     List<WebElement> tdElements = trElement.findElements(By.tagName("td"));
+                    Row row = sheet.createRow(rowNum++);
+                    int colNum = 0;
                     for (WebElement tdElement : tdElements) {
-                        System.out.println(tdElement.getText());
+                        if (colNum<3){
+                            Cell cell = row.createCell(colNum++);
+                            cell.setCellValue((tdElement.getText()));
+                        }else {
+                            Cell cell = row.createCell(colNum++);
+                            cell.setCellValue((Integer.parseInt(tdElement.getText())));
+                        }
+
+//                        System.out.println(tdElement.getText());
                     }
+
                 }
+            }
+            try(FileOutputStream fileOut = new FileOutputStream("Proky.xlsx")){
+                workbook.write(fileOut);
             }
             driver.close();
         }catch (Exception e){
@@ -42,3 +62,5 @@ public class App
         }
     }
 }
+
+
